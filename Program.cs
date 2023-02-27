9f0b07421a7b9d93ca9b39907361155a3a90a684
tchoj <tchoj@CHOJAY>
2023-02-27 22:19:19 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICAssignments
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your surname");
+            string surName = Console.ReadLine();
+
+            string fullName = firstName + " " + surName;
+            string helloName = $"Hello , {firstName} {surName}";
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(helloName);
+            Console.ReadLine();
+
+
+        }
+    }
+}
